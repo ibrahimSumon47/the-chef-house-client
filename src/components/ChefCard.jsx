65d3161deea0c892d0d1e282b19e13c1e41dad22
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-05-03 17:53:24 +0600

    chef card added and responsive for small device

--- src/components/ChefCard.jsx
+++ src/components/ChefCard.jsx
@@ -1,19 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { FaRegHeart } from 'react-icons/fa';
 
 const ChefCard = () => {
-    const [chefDetailsCard, setChefDetailsCard] = useState([])
+  const [chefDetailsCard, setChefDetailsCard] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:5000/chef_details")
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error))
-    }, [])
-    return (
-        <div>
-            
-        </div>
-    );
+  useEffect(() => {
+    fetch("http://localhost:5000/chef_details")
+      .then((res) => res.json())
+      .then((data) => setChefDetailsCard(data))
+      .catch((error) => console.error(error));
+  }, []);
+  return (
+    <div>
+      <h2 className="text-4xl font-extrabold text-center py-10" style={{backgroundColor: "#273262"}}>Best <span style={{color: "#c69860"}}>Chefs</span> in Our <span style={{color: "#ffc811"}}>Chef House</span></h2>
+      <div className="md:grid grid-cols-2 place-items-center place-self-auto px-14 py-5 md:px-10 bg-white">
+        {chefDetailsCard.map(({id, chef_picture, chef_name, experience, bio_description, Likes}) => (
+          <div key={id}>
+            <div className="card lg:card-side bg-base-100 shadow-xl my-5 mx-5">
+              <figure>
+                <img
+                  className="w-96 h-auto md:h-96"
+                  src={chef_picture}
+                  alt="Chef"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{chef_name}</h2>
+                <p>Experience: {experience}</p>
+                <h3 className="w-52">Bio: {bio_description}</h3>
+                <div className="">
+                  <div className="flex mt-5">
+                    <p className="">Likes: {Likes}</p>
+                    <button className=""><FaRegHeart/></button>
+
+                  </div>
+                  <button className="btn btn-primary mt-5 w-56">View Recipes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ChefCard;
